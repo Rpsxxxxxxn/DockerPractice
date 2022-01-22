85d340ba9391afc5b2b55c828f6c641e3e0646cf
Rpsxxxxxxn <sakuramotti123@gmail.com>
2022-01-22 21:57:16 +0900

    投稿画面の作成

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -1,16 +1,55 @@
 -- dockerが起動したら入る場所
 use project_db;
 
--- ユーザ情報生成処理
+-- ユーザ情報
 CREATE TABLE IF NOT EXISTS user (
-    email VARCHAR(255) PRIMARY KEY NOT NULL,
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
     username VARCHAR(20) NOT NULL,
     password VARCHAR(255) NOT NULL,
     comment VARCHAR(255),
-)
--- CREATE TABLE IF NOT EXISTS user (
---     id INT(11) PRIMARY KEY AUTO_INCREMENT,
---     username VARCHAR(20) NOT NULL,
---     email VARCHAR(255) NOT NULL,
---     password VARCHAR(255) NOT NULL
--- )
+    auth_id INT(1) NOT NULL DEFAULT 1,
+    is_delete INT(1) NOT NULL DEFAULT 0,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- ユーザ認証情報
+CREATE TABLE IF NOT EXISTS user_authority (
+    id INT(1) PRIMARY KEY,
+    comment VARCHAR(50),
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 利用者、管理者
+INSERT INTO user_authority(id, comment) VALUES(1, "利用者");
+INSERT INTO user_authority(id, comment) VALUES(2, "管理者");
+INSERT INTO user_authority(id, comment) VALUES(3, "スタッフ");
+
+-- 投稿項目
+CREATE TABLE IF NOT EXISTS post_info (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    comment VARCHAR(255) NOT NULL,
+    post_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 投稿ライク機能
+CREATE TABLE IF NOT EXISTS post_like (
+    email VARCHAR(255) NOT NULL,
+    post_id INT(11) NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(email, post_id)
+);
+
+-- ユーザブロック機能
+CREATE TABLE IF NOT EXISTS user_block (
+    user_id INT(11) NOT NULL,
+    target_id INT(11) NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(user_id, target_id)
+);
--- web/app.py
+++ web/app.py
@@ -1,12 +1,17 @@
+from email import message
 import flask
-from datetime import timedelta 
-from crypt import methods
-from flask import jsonify, session, render_template,request, url_for, redirect
 import mysql.connector
+from flask import jsonify, session, render_template, request, url_for, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+from crypt import methods
+from datetime import timedelta 
 
+# ================================================
+# 環境設定
+# ================================================
 app = flask.Flask(__name__)
 app.secret_key = 'XXXXXXXXXXXXXXXXXXXXXX'
-app.permanent_session_lifetime = timedelta(minutes=60) 
+app.permanent_session_lifetime = timedelta(minutes=60)
 
 # データベース接続設定
 # host='mysql-service',
@@ -21,6 +26,10 @@ db_config = {
   'database': 'project_db'
 }
 
+# ================================================
+# アプリケーション
+# ================================================
+
 # root処理
 @app.route("/", methods=["GET"])
 def index():
@@ -30,88 +39,149 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
   if request.method == "GET":
-    session["name"]="name"
-    return render_template("login.html", title="test", name="ore")
+    return render_template("login.html", message="")
+
   elif request.method == "POST":
     email = request.form.get("email")
     password = request.form.get("password")
     
-    # データベースへ接続
     conn = mysql.connector.connect(**db_config)
     app.logger.info("MySQLConnectionInfo: %s", conn.is_connected())
 
-    if conn.is_connected() == True:
-      try:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM user WHERE email = %s AND password = %s", [email, password])
-        row = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        print(row)
-        if row is not None:
+    try:
+      conn = mysql.connector.connect(**db_config)
+      cursor = conn.cursor(prepared=True)
+      stmt = "SELECT * FROM user WHERE email = %s;"
+      cursor.execute(stmt, (email,))
+      row = cursor.fetchone()
+
+      if row is not None:
+        if check_password_hash(row[3], password):
           session["user"] = row
-          print("logined")
           return redirect(url_for("top"))
-      except mysql.connector.Error as err:
-        print("Something went wrong: {}".format(err))
-        return render_template("login.html", message="エラーが発生しました。")
+        else:
+          return render_template("login.html", message="パスワードが違います。")
+      else:
+        return render_template("login.html", message="そのメールアドレスは登録されていません。")
+    except mysql.connector.Error as err:
+      print("Something went wrong: {}".format(err))
+      return render_template("login.html", message="エラーが発生しました。")
+    finally:
+      cursor.close()
+      conn.close()
     
-    return render_template("login.html")
+    return render_template("login.html", message="")
 
 # 登録画面
 @app.route("/regist_user", methods=["GET", "POST"])
 def regist_user():
   if request.method == "GET":
-    return render_template("regist_user.html")
+    return render_template("regist_user.html", message="")
+
   elif request.method == "POST":
     username = request.form.get("username")
     email = request.form.get("email")
     password = request.form.get("password")
-    
-    conn = mysql.connector.connect(**db_config)
-    if conn.is_connected() == True:
-      try:
-        sql = "insert into user(username, email, password) values(%s, %s, %s);"
-        cursor = conn.cursor()
-        cursor.executemany(sql, [(username, email, password)])
-        conn.commit()
-        cursor.close()
-        conn.close()
-      except mysql.connector.Error as err:
-        print("Something went wrong: {}".format(err))
-        return render_template("regist_user.html", message="エラーが発生しました。")
-    return redirect(url_for("login"))
+    password_confirm = request.form.get("check_password")
 
+    if password != password_confirm:
+      return render_template("regist_user.html", message="確認パスワードが違います。")
+
+    password_hash = generate_password_hash(password)
+
+    try:
+      conn = mysql.connector.connect(**db_config)
+      cursor = conn.cursor(prepared=True)
+
+      stmt = "SELECT * FROM user WHERE email = ?"
+      cursor.execute(stmt, (email,))
+      row = cursor.fetchone()
+
+      if row is not None:
+        return render_template("regist_user.html", message="このメールアドレスは既に登録されています。")
+
+      stmt = "insert into user(email, username, password) values(?, ?, ?);"
+      cursor.execute(stmt, (email, username, password_hash))
+      conn.commit()
+
+    except mysql.connector.Error as err:
+      conn.rollback()
+      print("Something went wrong: {}".format(err))
+      return render_template("regist_user.html", message="システムエラーが発生しました。")
+    finally:
+      cursor.close()
+      conn.close()
+
+    return redirect(url_for("login"))
 
 # topページ
 @app.route("/top", methods=["GET"])
 def top():
   if request.method == "GET":
-    if session["user"] is not None:  
-      conn = mysql.connector.connect(**db_config)
-      cursor = conn.cursor()
-      cursor.close()
-      conn.close()
-      return render_template("top.html")
+    if "user" in session:
+      sql = " SELECT"
+      sql += "  user.username, "
+      sql += "  post_info.post_at,"
+      sql += "  post_info.comment"
+      sql += " FROM post_info"
+      sql += "  INNER JOIN user"
+      sql += "  ON user.email = post_info.email"
+      sql += " ORDER BY"
+      sql += "  post_info.post_at DESC"
+
+      try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor(prepared=True)
+        cursor.execute(sql)
+        list_data = cursor.fetchall()
+      except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        return render_template("regist_user.html", message="エラーが発生しました。")
+      finally:
+        cursor.close()
+        conn.close()
+      return render_template("top.html", list_data=list_data)
     else:
       return redirect(url_for("error_page"))
+      
 
 # ログアウト
 @app.route("/logout", methods=["GET", "POST"])
 def logout():
   if request.method == "GET":
-    if session["user"] is not None:
-      app.logger.info("[INFO] User: %s Logout", session["user"].email)
-      session["user"] = None
-      return redirect(url_for("login"))
-    else:
-      return redirect(url_for("login"))
+    session.pop("user", None)
+    session.clear()
+    return redirect(url_for("login"))
 
 # エラーページ
 @app.route("/error", methods=["GET"])
 def error_page():
   if request.method == "GET":
-    return render_template("error.index")
+    return redirect(url_for("login"))
+    # return render_template("error.html")
+
+# 投稿ページ
+@app.route("/post", methods=["GET", "POST"])
+def post():
+  if request.method == "GET":
+    return render_template("post.html")
+  elif request.method == "POST":
+    if "user" in session:
+      comment = request.form.get("comment")
+      try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor(prepared=True)
+        cursor.execute("INSERT INTO post_info(email, comment) VALUES(?, ?)", (session["user"][1], comment))
+        conn.commit()
+      except mysql.connector.Error as err:
+        print("Something went wrong: {}".format(err))
+        return render_template("post.html", message="エラーが発生しました。")
+      finally:
+        cursor.close()
+        conn.close()
+      return redirect(url_for("top"))
+    else:
+      return redirect(url_for("login"))
 
 # init
 if __name__ == "__main__":
